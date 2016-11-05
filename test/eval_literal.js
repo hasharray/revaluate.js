0d71360f02a4197a778b8dd942b7bf5246bd89d5
Casper Beyer <caspervonb@users.noreply.github.com>
2016-11-06 03:46:48 +0800

    Add test to ensure basic literal evaluation works

---
+++ test/eval_literal.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var revaluate = require('..');
+
+var name = Date.now().toString(36) + '.js';
+for (var index = 0; index < 10; index++) {
+  var value = index;
+  var result = revaluate([
+    value,
+  ].join('\n'), name, function(output) {
+    return eval(output.code);
+  });
+
+  assert.equal(result, value);
+}
