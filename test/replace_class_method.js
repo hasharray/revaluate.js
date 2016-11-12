fbb1cb249ac2a5eba48225eee30f6976f4cb9c55
Casper Beyer <caspervonb@icloud.com>
2016-11-13 05:36:18 +0800

    Replace class methods

---
+++ test/replace_class_method.js
@@ -0,0 +1,42 @@
+var assert = require('assert');
+var revaluate = require('..');
+
+var name = Date.now().toString(36) + '.js';
+var objects = [];
+
+for (var i = 0; i < 10; i++) {
+  var value = i;
+  var result = revaluate([
+    '(class Class {',
+    '  fn() {',
+    '    return ' + value + ';',
+    '  }',
+    '})',
+  ].join('\n'), name, function(output) {
+    return eval(output.toString());
+  });
+
+  var object = new result();
+  objects.push(object);
+
+  for (var j = 0; j < objects.length; j++) {
+    assert.equal(objects[j].fn(), value);
+  }
+}
+
+for (var i = 0; i < objects.length; i++) {
+  for (var j = 0; j < objects.length; j++) {
+    assert.equal(objects[i].fn(), objects[j].fn());
+  }
+}
+
+revaluate([
+  '(class Class {',
+  '})',
+].join('\n'), name, function(output) {
+  return eval(output.toString());
+});
+
+for (var i = 0; i < objects.length; i++) {
+  assert.equal(objects[i].fn(), undefined);
+}
