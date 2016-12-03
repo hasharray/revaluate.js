dc10bb4114dba12630f8e51a4d372637622aea2f
Casper Beyer <caspervonb@icloud.com>
2016-12-03 07:20:13 +0100

    Add new methods to cached classes

---
+++ test/add_class_declaration_static_method.js
@@ -0,0 +1,36 @@
+var assert = require('assert');
+var revaluate = require('..');
+
+var name = Date.now().toString(36) + '.js';
+var result = [];
+
+for (var i = 0; i < 10; i++) {
+  var value = i;
+
+  var cls = revaluate([
+    'class Class {',
+    '}',
+    'Class',
+  ].join('\n'), name, function(output) {
+    return eval(output.toString());
+  });
+
+  result.push(cls);
+}
+
+revaluate([
+  'class Class {',
+  '  static fn() {',
+  '    return 0;',
+  '  }',
+  '}',
+  'Class',
+].join('\n'), name, function(output) {
+  return eval(output.toString());
+});
+
+for (var i = 0; i < result.length; i++) {
+  for (var j = 0; j < result.length; j++) {
+    assert.ok(result[i].fn);
+  }
+}
