7c9c48f9e93fc8631fac7961f10add0cf634bfdd
Casper Beyer <caspervonb@icloud.com>
2016-12-11 13:31:29 +0800

    Cache new expressions

---
+++ test/cache_new_expression.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var revaluate = require('..');
+
+var result = [];
+for (var i = 0; i < 10; i++) {
+  var obj = revaluate([
+    '(new function Ctor() {})',
+  ].join('\n'), __filename, function(output) {
+    return eval(output.toString());
+  });
+
+  result.push(obj);
+}
+
+for (var i = 0; i < result.length; i++) {
+  for (var j = 0; j < result.length; j++) {
+    assert.equal(result[i], result[j]);
+  }
+}
