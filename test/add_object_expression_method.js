feb62c3c2cc7debc5564960bd780dc1a44a6d259
Casper Beyer <caspervonb@icloud.com>
2016-12-11 11:24:27 +0800

    Add new properties to cached objects

---
+++ test/add_object_expression_method.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var revaluate = require('..');
+
+var name = Date.now().toString(36) + '.js';
+var result = [];
+
+for (var i = 0; i < 10; i++) {
+  var value = i;
+
+  var obj = revaluate([
+    '({',
+    '})',
+  ].join('\n'), name, function(output) {
+    return eval(output.toString());
+  });
+
+  result.push(obj);
+}
+
+revaluate([
+  '({',
+  '  fn: function() {}',
+  '})',
+].join('\n'), name, function(output) {
+  return eval(output.toString());
+});
+
+for (var i = 0; i < result.length; i++) {
+  for (var j = 0; j < result.length; j++) {
+    assert.ok(result[i].fn);
+  }
+}
